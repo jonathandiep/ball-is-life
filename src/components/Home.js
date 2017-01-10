8ec235521058e24ddb03bf96426266750e547b05
Jon Diep <jdiep@avery.com>
2017-01-09 22:23:54 -0800

    Searching is functional

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import * as _ from 'lodash';
 
-//const players = require('nba/data/players.json');
+const players = require('nba/data/players.json');
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = {
+      term: '',
+      players,
+      results: []
+    };
 
     this.onChange = this.onChange.bind(this);
   }
 
+  // the term state should affect what values (names) should appear in the dropdown
   onChange(event) {
-    console.log(event.target.value);
     this.setState({ term: event.target.value });
+    // eslint-disable-next-line
+    this.setState({ results: this.state.players.filter((player) => {
+        if (player.firstName.includes(event.target.value) || player.lastName.includes(event.target.value)) {
+          return player;
+        }
+      })
+    });
   }
 
   render() {
+    let playersList = this.state.results.map(player => 
+      <div key={player.playerId}>
+        <Link to={`/player/${player.playerId}`}><li>{player.firstName} {player.lastName}</li></Link>
+      </div>
+    );
+
     return (
       <div>
         <form>
@@ -24,10 +43,13 @@ export default class Home extends Component {
             <input
               type="text"
               className="form-control"
-              placeholder="Search a player..."
+              placeholder="Search a player... (Capitalization matters)"
               onChange={this.onChange} />
             <div>
-              Search Dropdown goes here
+              Search Results:
+              <ul>
+                {playersList}
+              </ul>
             </div>
           </div>
         </form>
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Team from './components/Team';
+import Player from './components/Player';
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/team" component={Team} />
+      <Route path="/player/:playerId" component={Player} />
     </Route>
   </Router>,
   //<App />,
