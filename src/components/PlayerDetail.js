66b54e1ca4d2f58879b290f97042e4152d516f7e
Jon Diep <jdiep@avery.com>
2017-03-21 00:06:18 -0700

    Add picture

--- src/components/PlayerDetail.js
+++ src/components/PlayerDetail.js
@@ -8,7 +8,8 @@ function PlayerDetail({ player = {}, button }) {
       <div className="card">
         <h3 className="card-header">{player.firstName} {player.lastName}</h3>
         <div className="card-block">
-          <h4 className="card-title">{player.teamCity} {player.teamName} (#{player.jersey})</h4>
+          <img className="img-thumbnail" alt="player" src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player.playerId}.png`} />
+          <h5 className="card-title">{player.teamCity} {player.teamName} (#{player.jersey})</h5>
           <p className="card-text">Position: {player.position}</p>
           <p className="card-text">Height: {player.height}</p>
           <p className="card-text">Weight: {player.weight} lbs.</p>
