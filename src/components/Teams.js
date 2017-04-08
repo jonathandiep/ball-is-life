52f8c3e0584ad9344e8a6c116d185b2ae8b413ee
Jon Diep <jdiep@avery.com>
2017-04-08 12:32:45 -0700

    Add https

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -7,7 +7,7 @@ export default function Teams() {
   const displayTeams = teams.map(team =>
     <Link to={`/team/${team.teamId}`} key={team.teamId}>
       <div className="card">
-        <img className="card-img-top img-fluid mx-auto d-block" src={`http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/${team.abbreviation}.svg`} alt={team.teamName} />
+        <img className="card-img-top img-fluid mx-auto d-block" src={`https://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/${team.abbreviation}.svg`} alt={team.teamName} />
         <div className="card-block">
           <h4 className="text-center">{team.teamName}</h4>
         </div>
