fed2446989ab5bf4d24d5b9876a7bf6712b404ab
Jon Diep <jdiep@avery.com>
2017-04-16 18:46:42 -0700

    Hotfix: change path

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/build`));
 
 const players = require('nba/data/players.json');
 
